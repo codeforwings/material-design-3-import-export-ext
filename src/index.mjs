1b626c7e4f368754b621bcc4ce5e83b54cc4ca70
JC <jchan48@gmail.com>
2023-06-18 01:06:22 -0400

    updated the test to check for the exported info

--- src/index.mjs
+++ src/index.mjs
@@ -1,3 +1,64 @@
 /**
  * Maybe just for taking a screenshot(s)
  */
+
+/**
+ * Basic JSPath to extract the color values from Material Theme Design
+ * @param _document {Document}
+ * @return themeColors {MaterialThemeCoreColors} @link{MaterialThemeCoreColors}
+ * @sample
+ * Will be used in chrome... so will be converted to a string
+ */
+export function exportCoreColorsUsingChrome(_document){
+  //quick and dirty check... dont really needed
+  // check if document is defined or not (chrome) or use the document passed in
+  const windowDocument = document ?? _document;
+
+
+  //verify document
+  /**
+   * Copy and paste this into the console of the theme builder
+   * or can export to a file... but that's zipped...
+   * if it's just seed color i think it's also very different?
+   * https://m3.material.io/theme-builder#/custom?primary=#cba642
+   * @example
+   * export const defaultFromMaterials ={
+   *   primary: '#6750A4',
+   *   secondary: '#958da4',
+   *   tertiary: '#b58392',
+   *   neutral: '#938f94',
+   * }
+   */
+
+  /**
+   * Using JSPath from Chrome DevTools to extract the color values
+   * @param queryIndex {number} - 1,2,3,4 for primary,secondary,tertiary,neutral
+   * @param document {Document} - window.document (chrome)
+   * @return {string} - Color Hex Value - Example #6750A4
+   * @example
+   * const color = m3ExtractColorBy(1,document);
+   */
+  function m3ExtractColorBy(queryIndex,document){
+    const m3AttributeStyle = document.querySelector("body > mio-root > mio-theme-builder > theme-builder")
+      .shadowRoot.querySelector("main > root-page > custom-base")
+      .shadowRoot.querySelector("main > section.options > article > div:nth-child(2) > core-colors")
+      .shadowRoot.querySelector(`section > div.colors > div:nth-child(${queryIndex}) > core-color-input`) //guess and correct
+      .shadowRoot.querySelector("#root > color-input")
+      .shadowRoot.querySelector("div")
+      .getAttribute("style");//--value: #6750A4; --color: #6750A4; --size: 48px; flex-direction: row;
+    return m3AttributeStyle.match(/--value: (.+?);/)[1];
+  }
+  const M3KeyToQueryIndex = [
+    { key: 'primary', i: 1 },
+    { key: 'secondary', i: 2 },
+    { key: 'tertiary', i: 3 },
+    { key: 'neutral', i: 4 }
+  ];
+  const themeColors = {};
+  for (const { key, i } of M3KeyToQueryIndex) {
+    const color = m3ExtractColorBy(i,windowDocument);
+    themeColors[key] = color;
+  }
+  // console.log(themeColors);
+  return themeColors;
+}
--- tests/import-material-theme-pup.test.mjs
+++ tests/import-material-theme-pup.test.mjs
@@ -1,6 +1,6 @@
 // import { createRequire } from 'module';
 // const require = createRequire(import.meta.url);
-// import assert from 'node:assert';
+import assert from 'node:assert';
 import { describe, it} from 'mocha';
 import fs from 'node:fs';
 function writeToFile(fileName,data,space=2){
@@ -10,46 +10,26 @@ function writeToFile(fileName,data,space=2){
     typeof data === 'string' ? data :JSON.stringify(data,null,+space)
   );
 }
-import puppeteer from "puppeteer-core";
-import {runPuppeteerWithBrowser} from "../src/import-material-theme-pup.mjs";
-
+// import puppeteer from "puppeteer-core";
 import { Browser, Page } from 'puppeteer-core';
-import {sampleCoreColorsTheme} from "../lib/materialDesignThemeConstants.mjs";
+import {DefaultCoreColors, defaultFromMaterials, sampleCoreColorsTheme} from "../lib/materialDesignThemeConstants.mjs";
+import {initBrowserForPuppeteerCore, testExportCoreColors} from "../lib/import-material-theme-pup.test.utils.mjs";
+import {runPuppeteerWithBrowser} from "##/src/import-material-theme-pup.mjs";
+import {exportCoreColorsUsingChrome} from "##/src/index.mjs";
 
-/**
- * Runs Puppeteer with a specific version of Chrome.
- * Hardcoded for now. used for testing
- * @param headless {boolean | 'new'}
- * @returns {Promise<Browser>}
- */
-async function initBrowserForPuppeteerCore(headless=false){
-
-  let browser;
-  let executablePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
-  if (process.platform === 'win32') {
-    executablePath = "/Program\ Files/Google/Chrome/Application/chrome.exe"//works..
-  }
-  browser = await puppeteer.launch({
-    // headless: false,
-    headless,
-    executablePath,
-    userDataDir: "/c/selenium/ChromeProfile",
-    profileDirectory: "Profile 1",
-    ignoreDefaultArgs: ["--disable-extensions"],//this seems to be good. not sure what --enable-automation is though
-    args: [
-      "--load-extension=/selenium/ChromeProfile/Profile\ 1/Extensions/cjpalhdlnbpafiamejdnhcphjbkeiagm/1.48.4_0",
-    ]
-  });
-  return browser;
-
-
-}
 describe('import-material-theme-pup.test.mjs', function(){
-  it('runPuppeteerWithBrowser', async function(){
+  /**
+   * Also takes a screenshot... need to assert somehow
+   */
+  it('runPuppeteerWithBrowser headless', async function(){
     this.timeout(10000);
       try{
 
         // const browser = await initBrowserForPuppeteerCore();
+        //screenshot stuff
+        fs.mkdirSync('temp', { recursive: true});
+        fs.existsSync('temp/screenshot.png')
+          && fs.renameSync('temp/screenshot.png','temp/screenshot.tmp.png')
         let headless
         headless = 'new';
         const browser = await initBrowserForPuppeteerCore('new');
@@ -58,11 +38,12 @@ describe('import-material-theme-pup.test.mjs', function(){
          */
         let page = await runPuppeteerWithBrowser(sampleCoreColorsTheme,browser);
         await page.screenshot({path: 'temp/screenshot.png'});
-        console.log('screenshot');
         if(headless === false){//wait for 5 seconds if not headless
           await new Promise(resolve => setTimeout(resolve, 5000));
         }
         await browser.close();
+        assert.ok(fs.existsSync('temp/screenshot.png'));//maybe should check if updated, also create folder
+        fs.existsSync('temp/screenshot.tmp.png') && fs.unlinkSync('temp/screenshot.tmp.png');
 
       }catch (err) {
         // console.error(err);
@@ -70,4 +51,52 @@ describe('import-material-theme-pup.test.mjs', function(){
         throw err;
       }
   });
+
+  /**
+   * @example
+   * const serializedFunction = exportCoreColorsUsingChrome.toString();
+   * const deserializedFunction = eval(`(${serializedFunction})`);
+   */
+  it('serialize / stringify function',function(){
+    const actual = exportCoreColorsUsingChrome.toString()
+    assert.ok(/async function exportCoreColorsUsingChrome/.test(actual));//doesnt have export... good
+    // console.log(actual);
+
+  });
+  it('testExportCoreColors headless', async function(){
+    this.timeout(10000);
+      try{
+
+        // const browser = await initBrowserForPuppeteerCore();
+        //screenshot stuff
+        fs.mkdirSync('temp', { recursive: true});
+        fs.existsSync('temp/screenshot.png')
+          && fs.renameSync('temp/screenshot.png','temp/screenshot.tmp.png')
+        let headless
+        headless = 'new';
+        const browser = await initBrowserForPuppeteerCore('new');
+        /**
+         *
+         */
+        let {page,colors} = await testExportCoreColors(browser);
+        assert.deepStrictEqual(colors,DefaultCoreColors);
+        // console.log(colors);
+
+        /* screenshot and close */
+        await page.screenshot({path: 'temp/screenshot.png'});
+        if(headless === false){//wait for 5 seconds if not headless
+          await new Promise(resolve => setTimeout(resolve, 5000));
+        }
+        await page.close();
+        await browser.close();
+        assert.ok(fs.existsSync('temp/screenshot.png'));//maybe should check if updated, also create folder
+        fs.existsSync('temp/screenshot.tmp.png') && fs.unlinkSync('temp/screenshot.tmp.png');
+
+      }catch (err) {
+        // console.error(err);
+        // process.exit(1);
+        throw err;
+      }
+  });
+
 });
